1b96ff3f31928a2f823c224f391a215adef486ea
jimedro <jimedro.hermet@gmail.com>
2026-07-31 19:38:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -185,10 +185,10 @@
 
 	// Loading page
 	var loaderPage = function() {
-		setTimeout(function() {
-        $(".fh5co-loader").fadeOut("slow");
-        }, 1000); 
-	};
+    setTimeout(function() {
+        $(".fh5co-loader").addClass("fade-out");
+        }, 1000); // 1000 = 1 second delay
+    };
 
 	var counter = function() {
 		$('.js-counter').countTo({
